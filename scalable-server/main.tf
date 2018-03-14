e65b4b75c0807b23e602dac6d856147f06061fd5
Jon Schulman <jschulman@js-mbpro15.local>
2018-03-14 08:44:30 -0700

    updating scalable server

--- scalable-server/main.tf
+++ scalable-server/main.tf
@@ -1,42 +1,67 @@
 variable "server_port" {
-    description =   "The port the server will use for HTTP requests"
-    default     =   8080
+  description = "The port the server will use for HTTP requests"
+  default     = 8080
 }
 
 output "public_ip" {
-    value   =   "${aws_instance.example.public_ip}"
+  value = "${aws_instance.example.public_ip}"
 }
 
 output "url" {
-    value   =   "http://${aws_instance.example.public_ip}:${var.server_port}"
+  value = "http://${aws_instance.example.public_ip}:${var.server_port}"
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-    ami                         =   "ami-40d28157"
-    instance_type               =   "t2.micro"
-    vpc_security_group_ids      =   ["${aws_security_group.instance.id}"]   
-user_data   =   <<-EOF
-                #!/bin/bash
-                echo "Hello, World" > index.html
-                nohup busybox httpd -f -p "${var.server_port}" &
-                EOF
+resource "aws_launch_configuration" "example" {
+  ami             = "ami-40d28157"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
 
-    tags {
-        Name = "terraform.example"
-    }
+  user_data = <<-EOF
+                    #!/bin/bash
+                    echo "Hello, World" > index.html
+                    nohup busybox httpd -f -p "${var.server_port}" &
+                    EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_security_group" "instance" {
-    name    =   "terraform-example-instance"
+  name = "terraform-example-instance"
 
-    ingress {
-        from_port   =   "${var.server_port}"
-        to_port     =   "${var.server_port}"
-        protocol    =   "tcp"
-        cidr_blocks =   ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  min_size             = 2
+  max_size             = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+}
+
+data "aws_availability_zones" "all" {}
