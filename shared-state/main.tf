d6ab78c06419f583b7188c22fd3c52ec698f908c
Jon Schulman <jschulman@js-mbpro15.local>
2018-03-15 17:44:33 -0700

    added backends

--- shared-state/main.tf
+++ shared-state/main.tf
@@ -2,6 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+output "s3_bucket_arn" {
+  value = "${aws_s3_bucket.terraform_state.arn}"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "jms-terraform-shared-state"
+    key     = "global/s3/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = "true"
+  }
+}
+
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "jms-terraform-shared-state"
 
