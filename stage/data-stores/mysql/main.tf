2dd8667771a16fb2f7d047d3a028f91e902ede88
Jon Schulman <jschulman@js-mbpro15.local>
2018-03-16 17:12:10 -0700

    more rdbms coolness

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -5,21 +5,9 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket  = "jms-terraform-shared-state"
-    key     = "global/s3/terraform.tfstate"
+    key     = "stage/data-stores/mysql/terraform.tfstate"
     region  = "us-east-1"
     encrypt = "true"
-  }
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "jms-terraform-shared-state"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
   }
 }
 
